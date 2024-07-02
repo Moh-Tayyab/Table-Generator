fdda032a90291149da44eeca8fc3262376dadcf5
Muhammad-Tayyab <m.tayyab1263@gmail.com>
2024-07-02 19:55:22 +0500

    latest update

---
+++ README.md
@@ -0,0 +1,7 @@
+## `Table Generator Project`
+
+```
+A command-line app built with TypeScript, Node.js, Inquirer, and ChalkAnimation that generates tables with custom columns and data. 
+Users input table settings and data, and the app displays the table in the console with colorful animation, with optional file saving.
+
+```
---
+++ app.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer";
+import chalk, { chalkStderr } from "chalk";
+import chalkAnimation from "chalk-animation";
+
+function mainMenu() {
+  inquirer
+    .prompt({
+      type: "number",
+      name: "table",
+      message: chalkStderr.bgBlueBright(
+        "Enter a number to generate its multiplication table:  "
+      ),
+      validate: (input) => {
+        if (input >= 2) {
+          return true; // Valid input
+        } else {
+          return "Please enter a number greater than or equal to 2.";
+        }
+      },
+    })
+    .then((response) => {
+      if (response.table) {
+        tableCreated(response.table);
+      }
+    });
+}
+async function tableCreated(tableNo: number) {
+  for (let i = 1; i <= 10; i++) {
+    const animation = chalkAnimation.rainbow(
+      `${tableNo} x ${i} = ${tableNo * i}`
+    );
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    animation.stop(); // Stop the animation after each iteration
+  }
+}
+mainMenu();
